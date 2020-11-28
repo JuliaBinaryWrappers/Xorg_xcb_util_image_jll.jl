58dc16aba644fb07c2f6994e8d552c90d2ccc1b1
jlbuild <juliabuildbot@gmail.com>
2020-11-28 02:18:17 +0000

    Xorg_xcb_util_image_jll build 0.4.0+1

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -2,57 +2,15 @@
 export libxcb_image
 
 using Xorg_xcb_util_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-
-# Relative path to `libxcb_image`
-const libxcb_image_splitpath = ["lib", "libxcb-image.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libxcb_image_path = ""
-
-# libxcb_image-specific global declaration
-# This will be filled out by __init__()
-libxcb_image_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libxcb_image = "libxcb-image.so.0"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Xorg_xcb_util_image")
+JLLWrappers.@declare_library_product(libxcb_image, "libxcb-image.so.0")
 function __init__()
-    global artifact_dir = abspath(artifact"Xorg_xcb_util_image")
+    JLLWrappers.@generate_init_header(Xorg_xcb_util_jll)
+    JLLWrappers.@init_library_product(
+        libxcb_image,
+        "lib/libxcb-image.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Xorg_xcb_util_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Xorg_xcb_util_jll.LIBPATH_list,))
-
-    global libxcb_image_path = normpath(joinpath(artifact_dir, libxcb_image_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libxcb_image_handle = dlopen(libxcb_image_path)
-    push!(LIBPATH_list, dirname(libxcb_image_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
